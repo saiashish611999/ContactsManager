eaa65675f49d838bae219ddab75dcb70f74d536c
sai ashish pinnamsetty <saiashish611999@gmail.com>
2026-08-06 15:22:09 +0530

    ImplementedIndexActionMethodOfPersonsController

--- ContactsManager.UI/Controllers/PersonsController.cs
+++ ContactsManager.UI/Controllers/PersonsController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ContactsManager.Core.DataTransferObjects.PersonDtos;
+using ContactsManager.Core.Enums;
+using ContactsManager.Core.ServiceContracts;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ContactsManager.UI.Controllers;
 
@@ -6,10 +9,63 @@ namespace ContactsManager.UI.Controllers;
 [Route("[controller]")]
 public sealed class PersonsController: Controller
 {
+    private readonly ICountriesService countriesService;
+    private readonly IPersonsService personsService;
+
+    public PersonsController(
+        ICountriesService countriesService,
+        IPersonsService personsService)
+    {
+        this.countriesService = countriesService;
+
+        this.personsService = personsService;
+    }
+
+    #region Index
+
     [Route("/")]
     [Route("[action]")]
-    public async Task<IActionResult> Index()
+    [HttpGet]
+    public async Task<IActionResult> Index(
+        [FromQuery] string? searchBy,
+        [FromQuery] string? searchString,
+        [FromQuery] string? sortBy,
+        [FromQuery] SortOrder sortOrder)
     {
-        return View();
+        Dictionary<string, string> searchByList = new Dictionary<string, string>()
+        {
+            { "Name", nameof(PersonResponse.PersonName)},
+            { "Email", nameof(PersonResponse.EmailAddress)},
+            { "Gender", nameof(PersonResponse.Gender)},
+            { "Date Of Birth", nameof(PersonResponse.DateOfBirth)},
+            { "Age", nameof(PersonResponse.Age)},
+            { "country", nameof(PersonResponse.CountryName)},
+            { "Address", nameof(PersonResponse.Address)},
+            { "ReceiveNewsLetters", nameof(PersonResponse.ReceivesNewsLetters)}
+        };
+
+        ViewBag.SearchByList = searchByList;
+
+        List<PersonResponse> filteredPersons = await personsService.GetFilteredPersons(searchBy, searchString);
+
+        ViewBag.CurrentSearchBy = searchBy;
+
+        ViewBag.CurrentSearchString = searchString;
+
+        List<PersonResponse> sortedPersons = personsService.GetSortedPersons(
+            filteredPersons,
+            sortBy,
+            sortOrder);
+
+        ViewBag.CurrentSortBy = sortBy;
+
+        ViewBag.CurrentSortOrder = sortOrder;
+
+
+        return View("Index", sortedPersons);
     }
+    #endregion
+
+    #region Create
+    #endregion
 }
